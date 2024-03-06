60235fd32e06c57483652dac4ed8fc3c8ba2f81d
ETOMARU1210 <149168787+ETOMARU1210@users.noreply.github.com>
2024-03-06 21:00:56 +0900

    recoil-persistをいったんコメント

--- anime_project/src/atom/userState.js
+++ anime_project/src/atom/userState.js
@@ -1,15 +1,15 @@
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
+// import { recoilPersist } from "recoil-persist";
 
-const { persistAtom } = recoilPersist({
-	key: "user",
-	storage: sessionStorage
-});
+// const { persistAtom } = recoilPersist({
+// 	key: "user",
+// 	storage: sessionStorage
+// });
 
 const userState = atom({
   key: "user",
   default: {},
-  effects_UNSTABLE: [persistAtom]
+  // effects_UNSTABLE: [persistAtom]
 });
 
 export default userState;
